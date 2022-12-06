b4df88753eaeec3a8e1b4ad9e07d4c79ab31a76f
Rajesh-P-07 <rajesh.pachika@gmail.com>
2022-12-06 14:15:15 +0530

    updated counter

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+  const [count,setCount]=useState(0);
   return (
-    <div>Counter</div>
+    <>
+    <h2>Counter</h2>
+    <h1>Count: {count}</h1>
+    <button onClick={()=>setCount(count-2)}>DEC</button>
+    <button onClick={()=>setCount(count+2)}>INC</button>
+    </>
   )
 }
 
